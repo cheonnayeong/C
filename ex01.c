da188b2923e876f7e754abf9e5257500f08d45d9
ceonnayoung <nayoung109191@gmail.com>
2022-11-27 13:02:59 +0900

    four arithmetic operations

---
+++ ex01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char op;
+    int a, b;
+
+    printf("===계산기 프로그램===\n");
+    printf("계산식을 입력하세요.(예: 3 + 3)\n");
+    scanf("%d %c %d" , &a, &op, &b);
+
+    if(op=='+') printf("\n%d", a+b);
+    else if(op=='-') printf("\n%d", a-b);
+    else if(op=='*') printf("\n%d", a*b);
+    else if(op=='/') printf("\n%d", a/b);
+    else printf("\n잘못입력하셨습니다.");
+    
+    return 0;
+}
