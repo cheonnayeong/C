a9162c3e2abfdad5548d8d078d981fb0777d63a0
ceonnayoung <nayoung109191@gmail.com>
2022-12-08 08:11:12 +0900

    stairs(2)

---
+++ 3704.c
@@ -0,0 +1,20 @@
+//계단 오르기 2
+#include <stdio.h>
+
+int memo[100001] = {0,1,2,4,7};
+
+int f(int k){
+    
+    if(memo[k]) return memo[k];
+    return memo[k] = (f(k-1) + f(k-2) + f(k-3))%1000;
+    
+}
+int main()
+{
+    int n;
+    
+    scanf("%d", &n);
+    printf("%d", f(n)); 
+
+    return 0;
+}
